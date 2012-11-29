83af4e3998b8e510c3528ce0a09d0ed9c40c826e
Alexander Nestorov <alexandernst@gmail.com>
2012-11-29 10:04:46 +0100

    Small fix

--- example.php
+++ example.php
@@ -5,7 +5,6 @@ header('Content-Type: text/html; charset=UTF-8'); //Make it clear that we're goi
 $hb = "hb.exe ";
 $url = escapeshellarg("http://www.google.com"); //You probably want to get the URL from GET/POST or whatever other method you want.
 
-exec("$hb \"$url\"", $result);
-foreach($result as $line) echo $line;
+echo shell_exec("$hb $url");
 
 ?>
