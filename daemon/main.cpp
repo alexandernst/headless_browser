a529465959f5ea8be9f9d0cdab2249e443270fa4
Alexander Nestorov <alexandernst@gmail.com>
2013-02-14 14:03:45 +0100

    Useless

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -2,24 +2,9 @@
 #include "daemon.h"
 
 int main(int argc, char *argv[]){
-#ifdef QT_NO_DEBUG
-    QCoreApplication a(argc, argv);
-#else
     QApplication a(argc, argv);
-#endif
 
     Server *server = new Server("ipc");
-
-#ifdef QT_NO_DEBUG
-    QWebView *wv = new QWebView();
-    wv->setPage(webPage);
-    wv->show();
-
-    QWebInspector *webInspector = new QWebInspector();
-    webInspector->setPage(wv->page());
-    webInspector->setGeometry(2000, 600, 1300, 400);
-    webInspector->show();
-#endif
 
     QObject::connect(server, &Server::newMessageFromClient, [server](int clientID, QString message){
         Daemon *webPage = new Daemon();
