ce096552179e1b7c6ae526c9e1790edd60038429
Alexander Nestorov <alexandernst@gmail.com>
2013-02-14 14:03:56 +0100

    Update  PHP example

--- client/example.php
+++ client/example.php
@@ -2,9 +2,9 @@
 
 header('Content-Type: text/html; charset=UTF-8'); //Make it clear that we're going to output UTF-8
 
-$hb = "hb.exe ";
+$headless_browser = "client.exe ";
 $url = escapeshellarg("http://www.google.com"); //You probably want to get the URL from GET/POST or whatever other method you want.
 
-echo shell_exec("$hb $url");
+echo shell_exec("$headless_browser $url");
 
 ?>
